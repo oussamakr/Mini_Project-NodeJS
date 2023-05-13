8bfa07462a54e1bfab13e0f574b21a17b7976519
krayen oussama <oussama.krayen@outlook.com>
2023-05-13 13:04:05 +0200

    news

--- Routes/Order_Route.js
+++ Routes/Order_Route.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const Route = express.Router();
+const passport = require("../Config/passport");
 const {
   Creat_Order,
   Get_OrderList,
   Get_OrderDetails,
 } = require("../Controllers/OrderController");
-
+const Secure_Route = passport.authenticate("bearer", { session: false });
 // creat , get orders
 Route.post("/creat_order", Creat_Order);
-Route.get("/Get_order_list", Get_OrderList);
+Route.get("/Get_order_list", Secure_Route, Get_OrderList);
 Route.get("/Get_order_details/:orderId", Get_OrderDetails);
 
 module.exports = Route;
